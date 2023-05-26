a757925279fb095ee5cc0eeaf6c2fd14fd1c0049
GJMShrigley <gjmshrigley@gmail.com>
2023-05-26 20:02:21 +0100

    updated CSS for build

--- src/pages/LayoutPage.js
+++ src/pages/LayoutPage.js
@@ -1,10 +1,10 @@
-import "../app.css";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import { useSelector } from "react-redux";
 import Login from "../components/Login";
+import "../app.css";
 
 function LayoutPage() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
